a8e642ae7a06fdfe49249680ae65f2784b1cdf4d
Krisez <zhouchaoxing@cmiot.chinamobile.com>
2021-10-20 09:25:10 +0800

    init

---
+++ app/src/main/java/cn/krisez/collection/app/EditActivity.kt
@@ -0,0 +1,13 @@
+package cn.krisez.collection.app
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import cn.krisez.collection.app.databinding.ActivityEditBinding
+
+class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
